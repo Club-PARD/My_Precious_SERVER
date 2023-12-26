b515ea466eb2be8fa6d836dbc0a79974678a689a
seizethedaylyn <rlacofls294@gmail.com>
2023-12-26 15:20:22 +0900

    Create MoneyGloveConfig

---
+++ src/main/java/com/myprecious/moneyglove/config/MoneyGloveConfig.java
@@ -0,0 +1,24 @@
+package com.myprecious.moneyglove.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class MoneyGloveConfig {
+    @Bean
+    public OpenAPI openAPI() {
+        return new OpenAPI()
+                .components(new Components())
+                .info(apiInfo());
+    }
+
+    private Info apiInfo(){
+        return new Info()
+                .title("My Precious Money Glove")//E
+                .description("돈 내놔")
+                .version("1.0.0"); // version
+    }
+}
