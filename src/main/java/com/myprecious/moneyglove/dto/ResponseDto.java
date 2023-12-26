e80adc39a312f6c35a9f4956171086460fa438b2
seizethedaylyn <rlacofls294@gmail.com>
2023-12-26 14:57:35 +0900

    Create ResponseDto

---
+++ src/main/java/com/myprecious/moneyglove/dto/ResponseDto.java
@@ -0,0 +1,24 @@
+package com.myprecious.moneyglove.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+@Data
+@AllArgsConstructor(staticName = "set")
+public class ResponseDto<D> {
+    private boolean result;
+    private String message;  // 성공 실패 메세지
+    private D data;          // data 넣을거임
+    private int total;
+
+    public static <D> ResponseDto<D> setSuccess(String message, D data) {
+        return ResponseDto.set(true, message, data, 0);
+    }
+
+    public static <D> ResponseDto<D> setSuccess(String message, D data, int total) {
+        return ResponseDto.set(true, message, data, total);
+    }
+
+    public static <D> ResponseDto<D> setFailed(String message) {
+        return ResponseDto.set(false, message, null, 0);
+    }
+}
