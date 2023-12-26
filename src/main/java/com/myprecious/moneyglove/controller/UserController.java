6f2b106c46cf3c82f089a3ca436f2ee2c5185d29
seizethedaylyn <rlacofls294@gmail.com>
2023-12-27 08:41:55 +0900

    Update UserController

--- src/main/java/com/myprecious/moneyglove/controller/UserController.java
+++ src/main/java/com/myprecious/moneyglove/controller/UserController.java
@@ -1,2 +1,30 @@
-package com.myprecious.moneyglove.controller;public class UserController {
+package com.myprecious.moneyglove.controller;
+
+import com.myprecious.moneyglove.dto.ResponseDto;
+import com.myprecious.moneyglove.dto.User.UserCreateRequestDto;
+import com.myprecious.moneyglove.entity.UserEntity;
+import com.myprecious.moneyglove.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/moneyglove/user") // 공통되는 URL 묶기.
+public class UserController {
+    private final UserService userService;
+
+    @PostMapping("/join")
+    public ResponseDto<UserEntity> createUser(@RequestBody UserCreateRequestDto userRequest){
+        ResponseDto<UserEntity> result = userService.createUser(userRequest);
+        return result;
+    }
+
+    @GetMapping("/findAll")
+    public ResponseDto<List<UserEntity>> finAll(){
+        ResponseDto<List<UserEntity>> result = userService.findAll();
+        return result;
+    }
 }
+
