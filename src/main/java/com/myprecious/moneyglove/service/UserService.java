4c1857eb4ae1ec191269a57fe93a2ff34fe4a069
seizethedaylyn <rlacofls294@gmail.com>
2023-12-27 15:47:07 +0900

    update: dto name change

--- src/main/java/com/myprecious/moneyglove/service/UserService.java
+++ src/main/java/com/myprecious/moneyglove/service/UserService.java
@@ -1,10 +1,9 @@
 package com.myprecious.moneyglove.service;
 
 import com.myprecious.moneyglove.dto.ResponseDto;
-import com.myprecious.moneyglove.dto.User.UserCreateRequestDto;
+import com.myprecious.moneyglove.dto.User.UserRequest;
 import com.myprecious.moneyglove.entity.UserEntity;
 import com.myprecious.moneyglove.repository.UserRepository;
-import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -20,14 +19,14 @@ public class UserService {
         this.userRepository = userRepository;
     }
 
-    public ResponseDto<UserEntity> createUser(UserCreateRequestDto userCreateRequest) {
-        String userId = userCreateRequest.getUserId();
+    public ResponseDto<UserEntity> createUser(UserRequest userRequest) {
+        String userId = userRequest.getUserId();
         try {
             if (userRepository.existsByUserId(userId)) {
                 return ResponseDto.setFailed("같은 아이디의 유저 존재함. 이미 가입된 유저");
             }
 
-            UserEntity userEntity = userCreateRequest.toEntity();
+            UserEntity userEntity = userRequest.toEntity();
             userRepository.save(userEntity);
             return ResponseDto.setSuccess("유저 등록 완료", userEntity);
         } catch (Exception e) {
