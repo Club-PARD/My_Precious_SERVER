37ba45c1111fa69a5e9d677ab1354a5172c1d834
seizethedaylyn <rlacofls294@gmail.com>
2023-12-27 15:46:37 +0900

    update: @Column 수정

--- src/main/java/com/myprecious/moneyglove/entity/BoardEntity.java
+++ src/main/java/com/myprecious/moneyglove/entity/BoardEntity.java
@@ -21,24 +21,28 @@ public class BoardEntity extends BaseEntity {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(nullable = false, unique = true)
+    @Column(nullable = false)
     private String title;
-    @Column(nullable = false, unique = true)
+    @Column(nullable = false)
     private Float borrowMoney;
-    @Column(nullable = false, unique = true)
+    @Column(nullable = false)
     private Integer payYear;
-    @Column(nullable = false, unique = true)
+    @Column(nullable = false)
     private Integer payMonth;
-    @Column(nullable = false, unique = true)
+    @Column(nullable = false)
     private Integer payDay;
-    @Column(nullable = false, unique = true)
+    @Column(nullable = false)
     private String situation;
-    @Column(nullable = false, unique = true)
+    @Column(nullable = false)
     private String payWay;
+    @Column(nullable = false)
+    private String bank;
+    @Column(nullable = false)
+    private String bankAccount;
 
     @Builder
     public BoardEntity(String title, Float borrowMoney, Integer payYear, Integer payMonth,
-                       Integer payDay, String situation, String payWay ) {
+                       Integer payDay, String situation, String payWay, String bank, String bankAccount ) {
         this.title = title;
         this. borrowMoney = borrowMoney;
         this.payYear = payYear;
@@ -46,7 +50,8 @@ public class BoardEntity extends BaseEntity {
         this.payDay = payDay;
         this.situation = situation;
         this.payWay = payWay;
-
+        this.bank = bank;
+        this.bankAccount = bankAccount;
     }
 
 }
