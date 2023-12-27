30b662ca6cb4bf7e5b725bae8b41aff3c46307aa
seizethedaylyn <rlacofls294@gmail.com>
2023-12-27 15:45:43 +0900

    update: dto name change

--- src/main/java/com/myprecious/moneyglove/dto/User/UserResponse.java
+++ src/main/java/com/myprecious/moneyglove/dto/User/UserResponse.java
@@ -6,7 +6,7 @@ import lombok.Data;
 
 
 @Data
-public class UserResponseDto {
+public class UserResponse {
     private Long id;
     private String name;
     private String birth;
@@ -15,7 +15,7 @@ public class UserResponseDto {
     private String userId;
 
     @Builder
-    public UserResponseDto(UserEntity user){
+    public UserResponse(UserEntity user){
         this.id = user.getId();
         this.name = user.getName();
         this.birth = user.getBirth();
