a8eaaa7ef835a4e681ec8542f4f54dc79817d67f
seizethedaylyn <rlacofls294@gmail.com>
2023-12-27 12:52:15 +0900

    create: entity 생성 시간, 수정 시간 super class

--- src/main/java/com/myprecious/moneyglove/entity/BaseEntity.java
+++ src/main/java/com/myprecious/moneyglove/entity/BaseEntity.java
@@ -1,2 +1,21 @@
-package com.myprecious.moneyglove.entity;public class BaseEntity {
+package com.myprecious.moneyglove.entity;
+
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@Getter
+@MappedSuperclass // 수퍼클래스
+@EntityListeners(value = AuditingEntityListener.class) //자동
+public class BaseEntity {
+    @CreatedDate
+    private LocalDateTime regDate;
+
+    @LastModifiedDate
+    private LocalDateTime modDate;
 }
