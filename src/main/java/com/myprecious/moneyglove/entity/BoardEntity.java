49fde39433bfd574e09a71f619c16ba8b66cb510
seizethedaylyn <rlacofls294@gmail.com>
2023-12-27 15:09:53 +0900

    create: BoardEntity

---
+++ src/main/java/com/myprecious/moneyglove/entity/BoardEntity.java
@@ -0,0 +1,52 @@
+package com.myprecious.moneyglove.entity;
+
+import jakarta.persistence.*;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@EntityListeners(AuditingEntityListener.class)
+public class BoardEntity extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String title;
+    @Column(nullable = false, unique = true)
+    private Float borrowMoney;
+    @Column(nullable = false, unique = true)
+    private Integer payYear;
+    @Column(nullable = false, unique = true)
+    private Integer payMonth;
+    @Column(nullable = false, unique = true)
+    private Integer payDay;
+    @Column(nullable = false, unique = true)
+    private String situation;
+    @Column(nullable = false, unique = true)
+    private String payWay;
+
+    @Builder
+    public BoardEntity(String title, Float borrowMoney, Integer payYear, Integer payMonth,
+                       Integer payDay, String situation, String payWay ) {
+        this.title = title;
+        this. borrowMoney = borrowMoney;
+        this.payYear = payYear;
+        this.payMonth = payMonth;
+        this.payDay = payDay;
+        this.situation = situation;
+        this.payWay = payWay;
+
+    }
+
+}
