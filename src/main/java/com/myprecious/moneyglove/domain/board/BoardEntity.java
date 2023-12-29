1ce2a7c0fc9fea44b7f909d3c252ba56e787d7a2
seizethedaylyn <rlacofls294@gmail.com>
2023-12-29 14:38:36 +0900

    feat: board entity 생성

--- src/main/java/com/myprecious/moneyglove/domain/board/BoardEntity.java
+++ src/main/java/com/myprecious/moneyglove/domain/board/BoardEntity.java
@@ -1,60 +1,47 @@
-package com.myprecious.moneyglove.board;
+package com.myprecious.moneyglove.domain.board;
 
-import com.myprecious.moneyglove.entity.BaseEntity;
-import com.myprecious.moneyglove.user.UserEntity;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.myprecious.moneyglove.BaseEntity;
+import com.myprecious.moneyglove.domain.user.UserEntity;
 import jakarta.persistence.*;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.hibernate.annotations.ColumnDefault;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.LocalDate;
-import java.time.Period;
 
 @Entity
 @Getter
 @Setter
+@Table(name = "board_entity")
 @NoArgsConstructor
 @EntityListeners(AuditingEntityListener.class)
 public class BoardEntity extends BaseEntity {
-
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(nullable = false)
     private String title;
-
-    @Column(nullable = false)
-    private Float borrowMoney;
-
-    @Column(nullable = false)
+    private String borrowMoney;
     private String payDate;
-
-    @Column(nullable = false)
     private String situation;
-
-    @Column(nullable = false)
     private String payWay;
-
-    @Column(nullable = false)
     private String bank;
-
-    @Column(nullable = false)
     private String bankAccount;
+    private String statuses;
 
-    @Column(nullable = false)
-    @Enumerated(EnumType.STRING)
-    private Status status = Status.getDefault();
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "user_id")
     private UserEntity user;
 
     @Builder
-    public BoardEntity(String title, Float borrowMoney, String payDate, String situation, String payWay, String bank, String bankAccount ) {
+    public BoardEntity(String title, String borrowMoney, String payDate, String situation,
+                       String payWay, String bank, String bankAccount, String statuses, UserEntity user) {
         this.title = title;
         this.borrowMoney = borrowMoney;
         this.payDate = payDate;
@@ -62,29 +49,7 @@ public class BoardEntity extends BaseEntity {
         this.payWay = payWay;
         this.bank = bank;
         this.bankAccount = bankAccount;
-
+        this.statuses = statuses;
+        this.user = user;
     }
-
-    public enum Status {
-        RECEIVED("Received"),
-        NOT_RECEIVED("Not Received");
-
-        private final String label;
-
-        // Enum 생성자에 기본값을 할당
-        Status(String label) {
-            this.label = label;
-        }
-
-        public String getLabel() {
-            return label;
-        }
-
-        // 기본값을 반환하는 메서드 추가
-        public static Status getDefault() {
-            return NOT_RECEIVED;
-        }
-    }
-
-
 }
