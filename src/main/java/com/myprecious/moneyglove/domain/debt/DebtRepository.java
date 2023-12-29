f01d638e92461273cf1ab872791e656e0e72cd89
seizethedaylyn <rlacofls294@gmail.com>
2023-12-29 23:12:40 +0900

    feat: debt repository 생성

--- src/main/java/com/myprecious/moneyglove/domain/debt/DebtRepository.java
+++ src/main/java/com/myprecious/moneyglove/domain/debt/DebtRepository.java
@@ -1,2 +1,6 @@
-package com.myprecious.moneyglove.domain.debt;public interface DebtRepository {
+package com.myprecious.moneyglove.domain.debt;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface DebtRepository extends JpaRepository<DebtEntity, Long> {
 }
