cfc7e27b252026c61cd6db8f990d892a8dd05684
seizethedaylyn <rlacofls294@gmail.com>
2023-12-29 23:13:24 +0900

    feat: debt response dto 생성

--- src/main/java/com/myprecious/moneyglove/domain/debt/DebtResponse.java
+++ src/main/java/com/myprecious/moneyglove/domain/debt/DebtResponse.java
@@ -1,2 +1,41 @@
-package com.myprecious.moneyglove.domain.debt;public class DebtResponse {
+package com.myprecious.moneyglove.domain.debt;
+
+import com.myprecious.moneyglove.domain.board.BoardSimpleResponse;
+import com.myprecious.moneyglove.domain.user.UserEntity;
+import com.myprecious.moneyglove.domain.user.UserSimpleResponse;
+import lombok.AllArgsConstructor;
+import com.myprecious.moneyglove.domain.debt.DebtEntity.DebtStatus;
+import com.myprecious.moneyglove.domain.debt.DebtEntity.RepaymentStatus;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class DebtResponse {
+
+    private Long id;
+    private String lendMoney;
+    private String message;
+    private String bank;
+    private String bankAccount;
+    private DebtStatus debtStatus;
+    private RepaymentStatus repaymentStatus;
+
+    private UserSimpleResponse user;
+    private BoardSimpleResponse board;
+
+    public DebtResponse(DebtEntity debt){
+        this.id = debt.getId();
+        this.lendMoney = debt.getLendMoney();
+        this.message = debt.getMessage();
+        this.bank = debt.getBank();
+        this.bankAccount = debt.getBankAccount();
+        this.debtStatus = debt.getDebtStatus();
+        this.repaymentStatus = debt.getRepaymentStatus();
+
+        this.user = new UserSimpleResponse(debt.getUser());
+        this.board = new BoardSimpleResponse(debt.getBoard());
+    }
+
 }
