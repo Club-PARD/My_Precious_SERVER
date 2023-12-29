352c8d29b6202c27fb1e9dd661192ed7b344ed49
seizethedaylyn <rlacofls294@gmail.com>
2023-12-29 15:14:26 +0900

    feat: userId로 글 찾아오는 jpa

--- src/main/java/com/myprecious/moneyglove/domain/board/BoardRepository.java
+++ src/main/java/com/myprecious/moneyglove/domain/board/BoardRepository.java
@@ -3,5 +3,8 @@ package com.myprecious.moneyglove.domain.board;
 import com.myprecious.moneyglove.domain.board.BoardEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface BoardRepository extends JpaRepository<BoardEntity, Long> {
+    List<BoardEntity> findByUserId(Long userId);
 }
