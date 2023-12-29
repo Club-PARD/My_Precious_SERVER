310f22146461ac8cd1c912d06ef13c3e26f8c2e0
seizethedaylyn <rlacofls294@gmail.com>
2023-12-29 15:13:21 +0900

    feat: 해당 유저의 글 찾아오는 api, 해당 글의 정보 찾아오는 api

--- src/main/java/com/myprecious/moneyglove/domain/board/BoardController.java
+++ src/main/java/com/myprecious/moneyglove/domain/board/BoardController.java
@@ -4,6 +4,8 @@ import com.myprecious.moneyglove.dto.ResponseDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RequiredArgsConstructor
 @RestController
 @RequestMapping("/api/boards") // 공통되는 URL 묶기.
@@ -14,6 +16,16 @@ public class BoardController {
     @PostMapping("/{userId}")
     public ResponseDto<BoardResponse> createBoard(@PathVariable final Long userId, @RequestBody final BoardRequest request){
         ResponseDto<BoardResponse> result = boardService.createBoard(userId, request);
+        return result;
+    }
+    @GetMapping("/users/{userId}")
+    public ResponseDto<List<BoardResponse>> findAll(@PathVariable Long userId){
+        ResponseDto<List<BoardResponse>> result = boardService.findAll(userId);
+        return result;
+    }
+    @GetMapping("/{boardId}")
+    public ResponseDto<BoardResponse> findOne(@PathVariable Long boardId ){
+        ResponseDto<BoardResponse> result = boardService.findOne(boardId);
         return result;
     }
 }
