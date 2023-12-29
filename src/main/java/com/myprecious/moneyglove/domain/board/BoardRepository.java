0a06fc8dd3f8e7b95cce6e830be737f44e5de689
seizethedaylyn <rlacofls294@gmail.com>
2023-12-29 14:37:58 +0900

    feat: boardRepository 생성

--- src/main/java/com/myprecious/moneyglove/domain/board/BoardRepository.java
+++ src/main/java/com/myprecious/moneyglove/domain/board/BoardRepository.java
@@ -1,6 +1,6 @@
-package com.myprecious.moneyglove.board;
+package com.myprecious.moneyglove.domain.board;
 
-import com.myprecious.moneyglove.board.BoardEntity;
+import com.myprecious.moneyglove.domain.board.BoardEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface BoardRepository extends JpaRepository<BoardEntity, Long> {
