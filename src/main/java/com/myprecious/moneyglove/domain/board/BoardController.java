edae25aece5c0c40777dc1d45626905a1388e8cf
seizethedaylyn <rlacofls294@gmail.com>
2023-12-29 14:39:14 +0900

    feat: board 생성 api

--- src/main/java/com/myprecious/moneyglove/domain/board/BoardController.java
+++ src/main/java/com/myprecious/moneyglove/domain/board/BoardController.java
@@ -1,4 +1,19 @@
-package com.myprecious.moneyglove.board;
+package com.myprecious.moneyglove.domain.board;
 
+import com.myprecious.moneyglove.dto.ResponseDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/api/boards") // 공통되는 URL 묶기.
 public class BoardController {
+
+    private final BoardService boardService;
+
+    @PostMapping("/{userId}")
+    public ResponseDto<BoardResponse> createBoard(@PathVariable final Long userId, @RequestBody final BoardRequest request){
+        ResponseDto<BoardResponse> result = boardService.createBoard(userId, request);
+        return result;
+    }
 }
