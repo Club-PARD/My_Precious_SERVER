e3417d8d36865c5392fbed730de29e74d0fc1169
seizethedaylyn <rlacofls294@gmail.com>
2023-12-29 23:12:17 +0900

    feat: 파일 생성 및 debt 추가 api 생성

--- src/main/java/com/myprecious/moneyglove/domain/debt/DebtController.java
+++ src/main/java/com/myprecious/moneyglove/domain/debt/DebtController.java
@@ -1,2 +1,23 @@
-package com.myprecious.moneyglove.domain.debt;public class DebtContoller {
+package com.myprecious.moneyglove.domain.debt;
+
+import com.myprecious.moneyglove.domain.board.BoardRequest;
+import com.myprecious.moneyglove.domain.board.BoardResponse;
+import com.myprecious.moneyglove.domain.board.BoardService;
+import com.myprecious.moneyglove.dto.ResponseDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/api/debts") // 공통되는 URL 묶기.
+public class DebtController {
+
+    private final DebtSevice debtService;
+    @PostMapping("/boards/{boardId}/users/{userId}")
+    public ResponseDto<DebtResponse> createBoard(@PathVariable final Long userId,
+                                                 @PathVariable final Long boardId,
+                                                 @RequestBody final DebtRequest request){
+        ResponseDto<DebtResponse> result = debtService.createDebt(userId, boardId, request);
+        return result;
+    }
 }
