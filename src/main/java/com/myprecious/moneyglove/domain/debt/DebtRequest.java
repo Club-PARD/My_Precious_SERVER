3829af11892f838ac0bb705ca300cbf997603984
seizethedaylyn <rlacofls294@gmail.com>
2023-12-29 23:13:13 +0900

    feat: debt request dto 생성

--- src/main/java/com/myprecious/moneyglove/domain/debt/DebtRequest.java
+++ src/main/java/com/myprecious/moneyglove/domain/debt/DebtRequest.java
@@ -1,4 +1,26 @@
 package com.myprecious.moneyglove.domain.debt;
 
+import com.myprecious.moneyglove.domain.user.UserEntity;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
 public class DebtRequest {
+
+    private String lendMoney;
+    private String message;
+    private String bank;
+    private String bankAccount;
+
+//    public DebtEntity toEntity(){
+//        return DebtEntity.builder()
+//                .lendMoney(lendMoney)
+//                .message(message)
+//                .bank(bank)
+//                .bankAccount(bankAccount)
+//                .build();
+//    } // builder 패턴으로 선언
 }
