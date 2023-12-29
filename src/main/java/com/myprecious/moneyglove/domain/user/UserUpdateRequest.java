906729ca52bae56615b194fbdb7369c8b2185f24
seizethedaylyn <rlacofls294@gmail.com>
2023-12-29 14:20:19 +0900

    feat: User 회원정보 입력 dto 생성

--- src/main/java/com/myprecious/moneyglove/domain/user/UserUpdateRequest.java
+++ src/main/java/com/myprecious/moneyglove/domain/user/UserUpdateRequest.java
@@ -1,2 +1,14 @@
-package com.myprecious.moneyglove.domain.user;public class UserUpdate {
+package com.myprecious.moneyglove.domain.user;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserUpdateRequest {
+    private String name;
+    private String birth;
+    private String phoneNum;
 }
