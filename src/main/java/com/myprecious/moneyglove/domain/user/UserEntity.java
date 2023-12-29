c027d46bbf19d84507c790f5996be99c417594a5
seizethedaylyn <rlacofls294@gmail.com>
2023-12-29 14:24:26 +0900

    feat: update method 추가

--- src/main/java/com/myprecious/moneyglove/domain/user/UserEntity.java
+++ src/main/java/com/myprecious/moneyglove/domain/user/UserEntity.java
@@ -1,6 +1,8 @@
-package com.myprecious.moneyglove.user;
+package com.myprecious.moneyglove.domain.user;
 
-import com.myprecious.moneyglove.board.BoardEntity;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.myprecious.moneyglove.BaseEntity;
+import com.myprecious.moneyglove.domain.board.BoardEntity;
 import jakarta.persistence.*;
 import lombok.Builder;
 import lombok.Getter;
@@ -16,33 +18,25 @@ import java.util.List;
 @Setter
 @NoArgsConstructor
 @EntityListeners(AuditingEntityListener.class)
-public class UserEntity extends BaseEntity{
+public class UserEntity extends BaseEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(nullable = false)
+    private String userId; //파이어베이스에서 받는 id 값
     private String name;
-
-    @Column(nullable = false)
     private String birth;
-
-    @Column(nullable = false)
     private String phoneNum;
-
-    @Column(nullable = false, unique = true)
     private String gmailId;
 
-    @Column(nullable = false, unique = true)
-    private String userId; //파이어베이스에서 받는 id 값
-
+    @JsonIgnore
     @OneToMany(mappedBy = "user", cascade = CascadeType.PERSIST)
     private List<BoardEntity> boards = new ArrayList<>();
 
     @Builder
     public UserEntity(String name, String birth, String phoneNum, String gmailId,
-                      String userId){
+                      String userId) {
         this.name = name;
         this.birth = birth;
         this.phoneNum = phoneNum;
@@ -50,38 +44,20 @@ public class UserEntity extends BaseEntity{
         this.userId = userId;
     }
 
-//    public void update(String name, Integer num, String birth, String phoneNum, String gmailId,
-//                       String userId) {
-//        if (name != null) {
-//            this.name = name;
-//        }
-//
-//        if (birth != null) {
-//            this.birth = birth;
-//        }
-//
-//        if (phoneNum != null) {
-//            this.phoneNum = phoneNum;
-//        }
-//
-//        if (gmailId != null) {
-//            this.gmailId = gmailId;
-//        }
-//
-//    }
+    public void update(UserUpdateRequest request) {
+        if (name != null) {
+            this.name = request.getName();
+        }
+
+        if (birth != null) {
+            this.birth = request.getBirth();
+        }
+
+        if (phoneNum != null) {
+            this.phoneNum = request.getPhoneNum();
+        }
+
+    }
+
 }
-
-
-//    //엔티티 연결
-//    // 하나의 동아리는 여러개의 시트를 잡을 수 있음
-//    @OneToMany(mappedBy = "club", cascade = CascadeType.PERSIST)
-//    private List<Sheet> checkedSheet = new ArrayList<>();
-//
-//    // 동아리에 연결된 시트를 삭제
-//    public void delete() {
-//        for (Sheet sheet : checkedSheet) {
-//            sheet.setClub(null); // 동아리와의 연관 관계 제거
-//        }
-//        checkedSheet.clear(); // 리스트 비우기
-//    }
 
