125f0af12f5be7c9c0f1d2567cfaf9c9f0e66c4c
seizethedaylyn <rlacofls294@gmail.com>
2023-12-29 14:21:06 +0900

    feat: update 기능 추가

--- src/main/java/com/myprecious/moneyglove/domain/user/UserService.java
+++ src/main/java/com/myprecious/moneyglove/domain/user/UserService.java
@@ -1,6 +1,7 @@
-package com.myprecious.moneyglove.user;
+package com.myprecious.moneyglove.domain.user;
 
 import com.myprecious.moneyglove.dto.ResponseDto;
+import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -21,9 +22,9 @@ public class UserService {
         String userId = userRequest.getUserId();
         try {
             if (userRepository.existsByUserId(userId)) {
-                return ResponseDto.setFailed("같은 아이디의 유저 존재함. 이미 가입된 유저");
+                UserEntity existedUser = userRepository.findByUserId(userId);
+                return ResponseDto.setFailed("같은 아이디의 유저 존재함. 이미 가입된 유저", existedUser);
             }
-
             UserEntity userEntity = userRequest.toEntity();
             userRepository.save(userEntity);
             return ResponseDto.setSuccess("유저 등록 완료", userEntity);
@@ -44,6 +45,29 @@ public class UserService {
                 return ResponseDto.setFailed("리스트가 비어있습니다."); //아무것도 없을 때
             int total = userResponses.size();
             return ResponseDto.setSuccess("유저 리스트 입니다",userResponses, total);
+        }catch(Exception e){
+            e.printStackTrace();
+            return ResponseDto.setFailed("데이터 베이스 오류");
+        }
+    }
+
+    @Transactional
+    public ResponseDto<UserEntity> updateUser(Long id, UserUpdateRequest request) {
+        UserEntity user;
+        try{
+            user = userRepository.findById(id).get();
+            if (!userRepository.existsById(id)) {
+                return ResponseDto.setFailed("해당 이름의 유저가 없습니다.");
+            }
+            if(!request.getName().isEmpty())
+                user.setName(request.getName()); //이름 수정
+            if(request.getBirth() != null)
+                user.setBirth(request.getBirth()); // 생일 수정
+            if(request.getPhoneNum() != null)
+                user.setPhoneNum(request.getPhoneNum()); // 생일 수정
+            user.update(request);
+            return ResponseDto.setSuccess("성공적으로 업데이트 되었습니다", user);
+
         }catch(Exception e){
             e.printStackTrace();
             return ResponseDto.setFailed("데이터 베이스 오류");
