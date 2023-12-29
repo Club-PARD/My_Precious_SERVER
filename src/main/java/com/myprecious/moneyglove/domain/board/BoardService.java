36d90c6bc8e4291be7e8c13f1b0c3c9e99f76d49
seizethedaylyn <rlacofls294@gmail.com>
2023-12-29 14:33:44 +0900

    feat: board create service

--- src/main/java/com/myprecious/moneyglove/domain/board/BoardService.java
+++ src/main/java/com/myprecious/moneyglove/domain/board/BoardService.java
@@ -1,31 +1,85 @@
-package com.myprecious.moneyglove.service;
+package com.myprecious.moneyglove.domain.board;
 
-import com.myprecious.moneyglove.board.BoardRepository;
+import com.myprecious.moneyglove.domain.board.BoardRepository;
+import com.myprecious.moneyglove.domain.user.UserEntity;
+import com.myprecious.moneyglove.domain.user.UserRepository;
+import com.myprecious.moneyglove.domain.user.UserRequest;
+import com.myprecious.moneyglove.dto.ResponseDto;
+import jakarta.transaction.Transactional;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.catalina.User;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
+@Slf4j
 @Service
 public class BoardService {
     private final BoardRepository boardRepository;
+    private final UserRepository userRepository;
 
     @Autowired
-    public BoardService(BoardRepository boardRepository) {
+    public BoardService(BoardRepository boardRepository, UserRepository userRepository) {
         this.boardRepository = boardRepository;
+        this.userRepository = userRepository;
     }
 
-//    public ResponseDto<BoardEntity> createBoard(BoardRequest boardRequest) {
-//        String userId = userRequest.getUserId();
-//        try {
-//            if (userRepository.existsByUserId(userId)) {
-//                return ResponseDto.setFailed("같은 아이디의 유저 존재함. 이미 가입된 유저");
-//            }
-//
-//            UserEntity userEntity = userRequest.toEntity();
-//            userRepository.save(userEntity);
-//            return ResponseDto.setSuccess("유저 등록 완료", userEntity);
-//        } catch (Exception e) {
+    public ResponseDto<BoardResponse> createBoard(Long id, BoardRequest request) {
+        try {
+            UserEntity user = userRepository.findById(id).get();
+            BoardEntity board = BoardEntity.builder()
+                    .title(request.getTitle())
+                    .situation(request.getSituation())
+                    .payDate(request.getPayDate())
+                    .payWay(request.getPayWay())
+                    .borrowMoney(request.getBorrowMoney())
+                    .bank(request.getBank())
+                    .bankAccount(request.getBankAccount())
+                    .statuses("NOT_RECEIVED")
+                    .user(user)
+                    .build();
+            try{
+                boardRepository.save(board);
+                BoardResponse result = new BoardResponse(board);
+                return ResponseDto.setSuccess("Successfully create posting",result);
+            } catch (Exception e){
+                e.printStackTrace();
+                return ResponseDto.setFailed("DB Error - Failed to create posting.");
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            return ResponseDto.setFailed("유저 엄슴");
+        }
+    }
+
+//    public ResponseDto<BoardResponse> findOne(Long boardId) {
+//        BoardResponse boardResponse;
+//        try{
+//            boardResponse = boardRepository.findById(boardId).get();
+//            return ResponseDto.setSuccess("동아리 찾기 성공!", boardResponse);
+//        }catch(Exception e){
 //            e.printStackTrace();
-//            return ResponseDto.setFailed("Database error");
+//            return ResponseDto.setFailed("데이터 베이스 오류");
 //        }
 //    }
+
+//    @Transactional
+//    public Sheet checkSheet(Long sheetId, String clubName) {
+//        Sheet sheet = sheetRepository.findById(sheetId)
+//                .orElseThrow(() -> new IllegalArgumentException("시트가 존재하지 않습니다."));
+//
+//        if (sheet.isChecked()) {
+//            throw new IllegalStateException("이미 빌려진 시트입니다.");
+//        }
+//
+//        Club club = clubRepository.findByName(clubName)
+//                .orElseThrow(() -> new IllegalArgumentException("동아리가 존재하지 않습니다."));
+//
+//        sheet.setClub(club);
+//        club.getCheckedSheet().add(sheet);
+//
+//        return sheetRepository.save(sheet);
+//    }
+
 }
