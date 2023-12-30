60de6534040e726013188c73b5b0e72220bd68c4
seizethedaylyn <rlacofls294@gmail.com>
2023-12-31 01:42:37 +0900

    feat: board 작성한 user 정보 출력

--- src/main/java/com/myprecious/moneyglove/domain/board/BoardSimpleResponse.java
+++ src/main/java/com/myprecious/moneyglove/domain/board/BoardSimpleResponse.java
@@ -1,5 +1,6 @@
 package com.myprecious.moneyglove.domain.board;
 
+import com.myprecious.moneyglove.domain.user.UserSimpleResponse;
 import lombok.Data;
 
 @Data
@@ -9,6 +10,7 @@ public class BoardSimpleResponse {
     private String payDate;
     private String bank;
     private String bankAccount;
+    private UserSimpleResponse userSimpleResponse;
 
     public BoardSimpleResponse(BoardEntity board) {
         this.title = board.getTitle();
@@ -16,5 +18,6 @@ public class BoardSimpleResponse {
         this.payDate = board.getPayDate();
         this.bank = board.getBank();
         this.bankAccount = board.getBankAccount();
+        this.userSimpleResponse = new UserSimpleResponse(board.getUser());
     }
 }
