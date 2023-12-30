037c316cf6c250acc67a986d063e71b6b60c2206
seizethedaylyn <rlacofls294@gmail.com>
2023-12-30 16:27:05 +0900

    feat: jpa 추가

--- src/main/java/com/myprecious/moneyglove/domain/debt/DebtRepository.java
+++ src/main/java/com/myprecious/moneyglove/domain/debt/DebtRepository.java
@@ -1,6 +1,10 @@
 package com.myprecious.moneyglove.domain.debt;
 
+import com.myprecious.moneyglove.domain.board.BoardEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface DebtRepository extends JpaRepository<DebtEntity, Long> {
+    List<DebtEntity> findByBoardId(Long boardId);
 }
