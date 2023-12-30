b1423e83f7e48ac4566b7c55e670e97750e7bc51
seizethedaylyn <rlacofls294@gmail.com>
2023-12-30 19:04:55 +0900

    feat: 해당 게시물에 친구들이 빌려준 돈 response

--- src/main/java/com/myprecious/moneyglove/domain/debt/DebtLendMoneyResponse.java
+++ src/main/java/com/myprecious/moneyglove/domain/debt/DebtLendMoneyResponse.java
@@ -1,2 +1,14 @@
-package com.myprecious.moneyglove.domain.debt;public class DebtLendMoneyResponse {
+package com.myprecious.moneyglove.domain.debt;
+
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+public class DebtLendMoneyResponse {
+    private String lendMoney;
+
+    public DebtLendMoneyResponse(DebtEntity debt) {
+        this.lendMoney = debt.getLendMoney();
+    }
 }
