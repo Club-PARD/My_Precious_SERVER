b244c76faf8e85794379a3ca41c6f4f36c43a96b
seizethedaylyn <rlacofls294@gmail.com>
2023-12-30 19:04:13 +0900

    feat: jpa 추가

--- src/main/java/com/myprecious/moneyglove/domain/debt/DebtRepository.java
+++ src/main/java/com/myprecious/moneyglove/domain/debt/DebtRepository.java
@@ -7,4 +7,5 @@ import java.util.List;
 
 public interface DebtRepository extends JpaRepository<DebtEntity, Long> {
     List<DebtEntity> findByBoardId(Long boardId);
+    List<DebtEntity> findByUser_Uid(String uid);
 }
