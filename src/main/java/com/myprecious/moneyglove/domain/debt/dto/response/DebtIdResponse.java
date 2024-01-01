1a3724c2f398842320b4be34f0b7bf1d3b00c169
seizethedaylyn <rlacofls294@gmail.com>
2024-01-01 17:12:23 +0900

    feat: debt id 만 response 하는 dto 생성

--- src/main/java/com/myprecious/moneyglove/domain/debt/dto/response/DebtIdResponse.java
+++ src/main/java/com/myprecious/moneyglove/domain/debt/dto/response/DebtIdResponse.java
@@ -1,2 +1,17 @@
-package com.myprecious.moneyglove.domain.debt.dto.response;public class DebtIdResponse {
+package com.myprecious.moneyglove.domain.debt.dto.response;
+
+import com.myprecious.moneyglove.domain.debt.DebtEntity;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class DebtIdResponse {
+    private Long id;
+
+    public DebtIdResponse(DebtEntity debtEntity){
+        this.id = debtEntity.getId();
+    }
 }
