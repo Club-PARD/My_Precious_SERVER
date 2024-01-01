bf955b468a8a32f8f7376b9748aa700207be6f75
seizethedaylyn <rlacofls294@gmail.com>
2024-01-02 02:39:21 +0900

    feat: d-day만 response - 추후 삭제 될 가능성 있음

--- src/main/java/com/myprecious/moneyglove/domain/board/dto/response/BoardDDayResponse.java
+++ src/main/java/com/myprecious/moneyglove/domain/board/dto/response/BoardDDayResponse.java
@@ -1,2 +1,17 @@
-package com.myprecious.moneyglove.domain.board;public class BoardDdayResponse {
+package com.myprecious.moneyglove.domain.board.dto.response;
+
+import com.myprecious.moneyglove.domain.board.BoardEntity;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class BoardDDayResponse {
+    private Integer DDay;
+
+    public BoardDDayResponse(BoardEntity boardEntity){
+        this.DDay = boardEntity.getDDay();
+    }
 }
