eacd8265a0bc41492aa96a8b9ae0bb7ca13fd189
seizethedaylyn <rlacofls294@gmail.com>
2024-01-02 02:42:31 +0900

    feat: jpa 쿼리문 추가 - boardStatus, d-day로 찾기

--- src/main/java/com/myprecious/moneyglove/domain/board/BoardRepository.java
+++ src/main/java/com/myprecious/moneyglove/domain/board/BoardRepository.java
@@ -1,13 +1,11 @@
 package com.myprecious.moneyglove.domain.board;
 
-import com.myprecious.moneyglove.domain.board.BoardEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
 public interface BoardRepository extends JpaRepository<BoardEntity, Long> {
     List<BoardEntity> findByUser_Uid(String uid);
-    BoardEntity findByBoardStatus(BoardEntity.BoardStatus boardStatus);
-
-    List<BoardEntity> findByDDay(Integer day);
+    List<BoardEntity> findByBoardStatus(BoardEntity.BoardStatus boardStatus);
+    List<BoardEntity> findBydDay(Integer dDay);
 }
