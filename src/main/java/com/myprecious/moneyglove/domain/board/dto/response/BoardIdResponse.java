02a9bfb109be1201a408fe77807af4dab44bbc43
seizethedaylyn <rlacofls294@gmail.com>
2024-01-02 02:36:18 +0900

    feat: create board id response dto

--- src/main/java/com/myprecious/moneyglove/domain/board/dto/response/BoardIdResponse.java
+++ src/main/java/com/myprecious/moneyglove/domain/board/dto/response/BoardIdResponse.java
@@ -1,2 +1,16 @@
-package com.myprecious.moneyglove.domain.board;public class BoardIdResponse {
+package com.myprecious.moneyglove.domain.board.dto.response;
+
+import com.myprecious.moneyglove.domain.board.BoardEntity;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class BoardIdResponse {
+    private Long id;
+    public BoardIdResponse(BoardEntity boardEntity){
+        this.id = boardEntity.getId();
+    }
 }
