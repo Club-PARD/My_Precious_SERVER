bf4d9d3d779c860525237301956b4830b5d50ae6
seizethedaylyn <rlacofls294@gmail.com>
2024-01-01 17:14:00 +0900

    feat: 해당 debt의 정보만 response하는 api

--- src/main/java/com/myprecious/moneyglove/domain/debt/DebtController.java
+++ src/main/java/com/myprecious/moneyglove/domain/debt/DebtController.java
@@ -3,6 +3,7 @@ package com.myprecious.moneyglove.domain.debt;
 import com.myprecious.moneyglove.common.ResponseDto;
 import com.myprecious.moneyglove.domain.debt.dto.request.DebtRequest;
 import com.myprecious.moneyglove.domain.debt.dto.request.DebtStatusRequest;
+import com.myprecious.moneyglove.domain.debt.dto.response.DebtIdResponse;
 import com.myprecious.moneyglove.domain.debt.dto.response.DebtResponse;
 import com.myprecious.moneyglove.domain.debt.dto.response.DebtStatusResponse;
 import com.myprecious.moneyglove.domain.debt.dto.response.RepaymentStatusResponse;
@@ -19,10 +20,10 @@ public class DebtController {
     private final DebtService debtService;
 
     @PostMapping("/boards/{boardId}/users/{uId}")
-    public ResponseDto<DebtResponse> createBoard(@PathVariable final String uId,
-                                                 @PathVariable final Long boardId,
-                                                 @RequestBody final DebtRequest request) {
-        ResponseDto<DebtResponse> result = debtService.createDebt(uId, boardId, request);
+    public ResponseDto<DebtIdResponse> createBoard(@PathVariable final String uId,
+                                                   @PathVariable final Long boardId,
+                                                   @RequestBody final DebtRequest request) {
+        ResponseDto<DebtIdResponse> result = debtService.createDebt(uId, boardId, request);
         return result;
     }
 
