6b22f819d0af1845d4361521b1275f4c54e33a31
seizethedaylyn <rlacofls294@gmail.com>
2024-01-06 09:34:46 +0900

    fix: mail 본문 수정

--- src/main/java/com/myprecious/moneyglove/domain/mail/service/MailService.java
+++ src/main/java/com/myprecious/moneyglove/domain/mail/service/MailService.java
@@ -60,11 +60,11 @@ public class MailService {
             mailHandler.setSubject("[MoneyGlove] 친구가 돈을 빌려줬어요! 힘들 때 도와준 친구에게 편지로 고마움을 표현해주세요.");
 
             String htmlContent = "<html><body>" +
-                    "<p>안녕하세요. 머니글러브입니다.</p>" +
-                    "<p>친구가 돈을 빌려줬어요!</p>" +
-                    "<p>지금 게시물 관리 페이지로 이동하여 확인하고 감사편지를 보내보세요.</p>" +
-                    "<p>감사편지 한장이 머글님의 친구들에게 큰 기쁨이 될거예요.</p>" +
-                    "<p>[만약 빌려준 돈과 실제로 계좌에 들어온 금액이 다르다면 moneyglove2024@gmail.com로 문의주세요.]</p>" +
+                    "<p>안녕하세요. 머니글러브입니다." +
+                    "<p>친구가 돈을 빌려줬어요!" +
+                    "<p>지금 게시물 관리 페이지로 이동하여 확인하고 감사 편지를 보내보세요." +
+                    "<p> 감사 편지 한 장이 머글님의 친구들에게 큰 기쁨이 될 거예요." +
+                    "<p>[만약 빌려준 돈과 실제로 계좌에 들어온 금액이 다르다면 moneyglove2024@gmail.com로 문의주세요.]>" +
                     "<p><a href=\"https://precious-relationship.web.app\">Money Glove</a></p>" +
                     "</body></html>";
 
@@ -90,11 +90,11 @@ public class MailService {
             mailHandler.setSubject("[MoneyGlove] 모든 돈을 갚으셨군요! 축하합니다.");
 
             String htmlContent = "<html><body>" +
-                    "<p>안녕하세요. 머니글러브입니다.</p>" +
-                    "<p>빌린 모든 금액을 갚으셨습니다.</p>" +
-                    "<p>머글님의 친구관계를 지키기 위한 책임감 있는 모습에 박수를 보내드립니다!!!</p>" +
-                    "<p>한때 힘들었던 시간 함께 도와줬던 친구들에게 감사하다고 한번 더 말해보는 것은 어떨까요?</p>" +
-                    "<p>지금까지 “돈도 지키고 우정도 지키자!” MoneyGlove를 이용해주셔서 감사합니다.</p>" +
+                    "<p>안녕하세요. 머니글러브입니다." +
+                    "<p>빌린 모든 금액을 갚으셨습니다." +
+                    "<p>머글님의 친구 관계를 지키기 위한 책임감 있는 모습에 박수를 보내드립니다!!!" +
+                    "<p>한때 힘들었던 시간 함께 도와줬던 친구들에게 감사하다고 한 번 더 말해보는 것은 어떨까요?" +
+                    "<p>지금까지 “돈도 지키고 우정도 지키자!” MoneyGlove를 이용해 주셔서 감사합니다." +
                     "<p><a href=\"https://precious-relationship.web.app\">Money Glove</a></p>" +
                     "</body></html>";
 
@@ -122,8 +122,8 @@ public class MailService {
             String htmlContent = "<html><body>" +
                     "<p>안녕하세요. 머니글러브입니다.</p>" +
                     "<p>친구가 돈을 갚았다고 합니다.</p>" +
-                    "<p>지금 바로 확인 후에 확인 버튼을 눌러주세요!!</p>" +
-                    "<p>MoneyGlove를 통해 친구와 꾸준히 소통해보아요 희힣ㅅ.</p>" +
+                    "<p>지금 바로 확인 후에 확인 버튼을 눌러주세요!</p>" +
+                    "<p>MoneyGlove를 통해 친구와 꾸준히 소통해 주세요!</p>" +
                     "<p><a href=\"https://precious-relationship.web.app\">Money Glove</a></p>" +
                     "</body></html>";
 
